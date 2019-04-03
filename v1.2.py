5a144bc5ad8de7ec8a003d99c4c667282c9e3b81
NoobZ2 <435884647@qq.com>
2019-04-03 16:49:20 +0800

    1

--- v1.2.py
+++ v1.2.py
@@ -6,6 +6,11 @@ from tkinter import filedialog
 import chardet
 import math
 import tkinter
+import numpy as np
+from tkinter.scrolledtext import ScrolledText
+import tkinter.font as tkFont
+import os
+from tkinter.messagebox import showinfo
 
 LOG_LINE_NUM = 0
 
@@ -15,16 +20,23 @@ LOG_LINE_NUM = 0
 class MY_GUI():
 
 
+
+
     def __init__(self,init_window_name):
         self.init_window_name = init_window_name
-
+        self.max1=""
+        self.filename=""
 
 
     #设置窗口
     def set_init_window(self):
+        # 设置输入输出框字体
+        ft = tkFont.Font(family='宋体', size=15)
+
+
         self.init_window_name.title("电子病历标注工具_v1.2   ")           #窗口名
         #self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
-        self.init_window_name.geometry('1068x681+10+10')
+        self.init_window_name.geometry('1500x1000+10+10')
         #self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
         #self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
         #标签
@@ -34,17 +46,27 @@ class MY_GUI():
         self.result_data_label.grid(row=0, column=12)
         self.log_label = Label(self.init_window_name, text="日志")
         self.log_label.grid(row=12, column=0)
+
         #文本框
 
-        self.init_data_Text = Text(self.init_window_name, width=67, height=35)  #原始数据录入框
-        #self.init_data_Text.
+        self.init_data_Text = ScrolledText(self.init_window_name, width=67, height=35,font=ft)  #原始数据录入框
         self.init_data_Text.grid(row=1, column=0, rowspan=10, columnspan=10)
-        self.result_data_Text = Text(self.init_window_name, width=70, height=49)  #处理结果展示
+        self.result_data_Text = ScrolledText(self.init_window_name, width=70, height=49,font=ft)  #处理结果展示
         self.result_data_Text.grid(row=1, column=12, rowspan=15, columnspan=10)
         self.log_data_Text = Text(self.init_window_name, width=66, height=9)  # 日志框
         self.log_data_Text.grid(row=13, column=0, columnspan=10)
         self.init_data_Text.bind("<Button-1>", self.button_start)
         self.init_data_Text.bind("<ButtonRelease-1>", self.button_end)
+
+
+
+
+
+
+
+
+
+
         #按钮
         # self.str_trans_to_md5_button = Button(self.init_window_name, text="字符串转MD5", bg="lightblue", width=10,command=self.str_trans_to_md5)  # 调用内部方法  加()为直接调用
         # self.str_trans_to_md5_button.grid(row=1, column=11)
@@ -61,20 +83,29 @@ class MY_GUI():
         self.output_button = Button(self.init_window_name,text="导出文件",bg="lightgreen",width=8,command=self.outputfile)
         self.output_button.grid(row=0,column=14)
         #标记解剖部位按钮
-        self.show_button = Button(self.init_window_name,text="解剖部位",width='8',command=self.show_jpbw)
+        self.show_button = Button(self.init_window_name,text="解剖部位",bg="lightblue",width='8',command=self.show_jpbw)
         self.show_button.grid(row=2, column=11)
         # 标记症状描述按钮
-        self.show_button = Button(self.init_window_name, text="症状描述", width='8', command=self.show_zzms)
+        self.show_button = Button(self.init_window_name, text="症状描述",bg="lightyellow", width='8', command=self.show_zzms)
         self.show_button.grid(row=3, column=11)
         # 标记独立症状按钮
-        self.show_button = Button(self.init_window_name, text="独立症状", width='8', command=self.show_dlzz)
+        self.show_button = Button(self.init_window_name, text="独立症状", bg="lightgreen",width='8', command=self.show_dlzz)
         self.show_button.grid(row=4, column=11)
         # 标记药物按钮
-        self.show_button = Button(self.init_window_name, text="药物", width='8', command=self.show_yw)
+        self.show_button = Button(self.init_window_name, text="药物",bg="red", width='8', command=self.show_yw)
         self.show_button.grid(row=5, column=11)
         # 标记手术按钮
-        self.show_button = Button(self.init_window_name, text="手术", width='8', command=self.show_ss)
+        self.show_button = Button(self.init_window_name, text="手术",bg="lightpink", width='8', command=self.show_ss)
         self.show_button.grid(row=6, column=11)
+        # 恢复操作按钮
+        self.recover_button = Button(self.init_window_name, text="恢复", width='8', command=self.recover)
+        self.recover_button.grid(row=0,column =15)
+        # 标注撤销功能ctrl+z实现
+        self.back_button = Button(self.init_window_name, text="撤销", width='8', command=self.backToHistory)
+        self.back_button.grid(row=0, column=16)
+        self.result_data_Text.bind('<Control-Key-z>',self.backToHistory)
+
+        self.result_data_Text.edit_separator()
 
     #功能函数
     def str_trans_to_md5(self):
@@ -110,6 +141,9 @@ class MY_GUI():
 
     #标记解剖部位
     def show_jpbw(self):
+        self.result_data_Text.edit_separator()
+
+
         start_index = str(s)
         start_index = start_index[2:]
         start_index = int(start_index)
@@ -117,18 +151,19 @@ class MY_GUI():
         end_index = str(e)
         end_index = end_index[2:]
         end_index = int(end_index)
+
         print(self.init_data_Text.selection_get()+"\t"+"解剖部位"+"\n")
         self.result_data_Text.insert(END,self.init_data_Text.selection_get()+"\t" + str(start_index) + "\t" + str(end_index) + "\t" +"解剖部位"+"\n")
         print(self.result_data_Text.get(END))
+        self.max1 =self.result_data_Text.get('1.0',END)
+        self.result_data_Text.edit_separator()
 
-    # 导出文件功能
-    def outputfile(self):
-        fname = filedialog.askopenfilename(title='打开文件', filetypes=[('All Files', '*')])
-        f = open(fname, 'w', encoding='utf-8', errors='ignore')
-        f.write(self.result_data_Text.get("1.0","end"))
+
+
 
     # 标记症状描述
-    def show_zzms(self):
+    def show_zzms(self,):
+        self.result_data_Text.edit_separator()
         start_index = str(s)
         start_index = start_index[2:]
         start_index = int(start_index)
@@ -137,11 +172,15 @@ class MY_GUI():
         end_index = end_index[2:]
         end_index = int(end_index)
 
+
         print(self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index) + "症状描述" + "\n")
-        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t"+ str(start_index) + "\t" + str(end_index)  + "症状描述" + "\n")
+        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index)  +"\t" + "症状描述" + "\n")
+        self.max1 = self.result_data_Text.get('1.0', END)
+        self.result_data_Text.edit_separator()
 
     # 标记独立症状
     def show_dlzz(self):
+        self.result_data_Text.edit_separator()
         start_index = str(s)
         start_index = start_index[2:]
         start_index = int(start_index)
@@ -149,12 +188,16 @@ class MY_GUI():
         end_index = str(e)
         end_index = end_index[2:]
         end_index = int(end_index)
+
         print(self.init_data_Text.selection_get() + "\t"+ str(start_index) + "\t" + str(end_index)  + "独立症状" + "\n")
-        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index) + "独立症状" + "\n")
+        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index) + "\t" +"独立症状" + "\n")
+        self.max1 = self.result_data_Text.get('1.0', END)
+        self.result_data_Text.edit_separator()
 
 
     # 标记药物
     def show_yw(self):
+        self.result_data_Text.edit_separator()
         start_index = str(s)
         start_index = start_index[2:]
         start_index = int(start_index)
@@ -162,12 +205,16 @@ class MY_GUI():
         end_index = str(e)
         end_index = end_index[2:]
         end_index = int(end_index)
+
         print(self.init_data_Text.selection_get() + "\t" + "药物" + "\n")
-        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index) + "药物" + "\n")
+        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t" + str(start_index) + "\t" + str(end_index) + "\t" +"药物" + "\n")
+        self.max1 = self.result_data_Text.get('1.0', END)
+        self.result_data_Text.edit_separator()
 
 
     # 标记手术
     def show_ss(self):
+        self.result_data_Text.edit_separator()
         start_index = str(s)
         start_index = start_index[2:]
         start_index = int(start_index)
@@ -175,8 +222,32 @@ class MY_GUI():
         end_index = str(e)
         end_index = end_index[2:]
         end_index = int(end_index)
+
         print(self.init_data_Text.selection_get() + "\t" + "手术" + "\n")
-        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t"+ str(start_index) + "\t" + str(end_index) + "手术" + "\n")
+        self.result_data_Text.insert(END, self.init_data_Text.selection_get() + "\t"+ str(start_index) + "\t" + str(end_index) + "\t" +"手术" + "\n")
+        self.max1 = self.result_data_Text.get('1.0', END)
+        self.result_data_Text.edit_separator()
+
+    #标注操作撤销功能
+    def callback(self,event):
+        # 每当有字符插入的时候，就自动插入一个分割符，主要是防止每次撤销的时候会全部撤销
+        self.result_data_Text.edit_separator()
+
+
+
+    def backToHistory(self):  #撤销操作
+        if len(self.result_data_Text.get('1.0','end'))!=0:
+            self.result_data_Text.edit_undo()
+        else:  #无字符时不能撤销
+            return
+
+    def recover(self):   #恢复操作
+        if len(self.max1) == len(self.result_data_Text.get('1.0',END)):
+            return
+        self.result_data_Text.edit_redo()
+
+
+
 
 
 
@@ -190,10 +261,38 @@ class MY_GUI():
 
     #打开文件功能
     def openfile(self):
+
         fname = filedialog.askopenfilename(title='打开文件', filetypes=[('All Files', '*')])
+        self.filename=os.path.basename(fname)
+        print(self.filename)
+
         f=open(fname,'r',encoding='utf-8',errors='ignore')
-        f_contet = f.read()
+        #对文本数据存储进数组，方便后续操作
+        line = f.readline()
+        data_list = []
+        while line:
+            num = list(map(str, line.split()))
+            data_list.append(num)
+            line = f.readline()
+        f.close()
+        data_array = np.array(data_list)
+
+
+        f_contet = data_array
         self.init_data_Text.insert(END,f_contet)
+
+
+    # 导出文件功能
+    def outputfile(self):
+        if self.filename!="":
+          os.chdir(r'E:\GitTest\untitled\文本标注1.1\Annoation')
+          f = open("ann"+self.filename, 'w', encoding='utf-8', errors='ignore')
+          f.write(self.result_data_Text.get("1.0", "end"))
+          showinfo(title="成功",message="标注文件已导出至Annoation文件夹")
+        else:
+            showinfo(title="错误",message="未找到指定文件")
+
+
 
 
 
